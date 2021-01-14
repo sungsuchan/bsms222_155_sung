230d60dcebadd82933da368c0eddb858b3be90b2
seongsuchan <tncksdl0805@gmail.com>
2021-01-14 19:36:22 +0900

    update

---
+++ python_basic1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+"""
+
+age = 22
+name = 'suchan'
+#%%
+print('{0} was {1} years old when he worte this book'.format(name,age))
+print('Why is {0} playing with that python?'.format(name))
+#%%
+print(f'{name} was {age} years old when he wrote this book')
+print(f'why is {name} playing with that python?')
+
+#%%
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+#%%
+print('a', end='')
+print('b', end='')
+#%% 
+print('a', end=' ')
+print('b', end =' ')
+print('c', end=' ')
+#%%
+print('this is the first line \nthis is the second line')
+print('this is the first line \t this is the second line')
+#%%
+print(R'new \n not line lol')
+
